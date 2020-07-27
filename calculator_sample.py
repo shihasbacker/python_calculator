f98110c0e6f7876be8cdcaa3024e2dcf49aac426
Shihas Backer <shihasbacker07@gmail.com>
2020-07-27 21:00:01 +0530

    screen added with disabled keyboard inputs

--- calculator_sample.py
+++ calculator_sample.py
@@ -1,7 +1,15 @@
+#calculator using python tkinter
 from tkinter import *
+
+#tk window
 window=Tk()
 window.geometry("500x500")
 window.title("calculator")
 window.configure(bg="#00004d")
+
+screen = Entry(window, font=('arial', 17, 'bold'), width=28, justify=RIGHT)
+screen.grid(row=0, column=0, columnspan=5, pady=1)
+screen.bind("<Key>", lambda e: "break")     # Disable characters from keyboard
+screen.insert(0, "0")
 
 window.mainloop()
