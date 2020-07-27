1750bb7dde65ed61ed633798b63daefe7158e111
Shihas Backer <shihasbacker07@gmail.com>
2020-07-27 16:25:36 +0530

    sample codes initialised

--- calculator_sample.py
+++ calculator_sample.py
@@ -1,3 +1,14 @@
 from tkinter import *
 window=Tk()
+window.geometry("500x500")
+window.title("calculator")
+window.configure(bg="red")
+button=Button(text="ok",width=30,height=30)
+label=Label(window,text="welcome")
+
+def hello():
+    print("button clicked")
+
+button.pack()
+label.pack()
 window.mainloop()
