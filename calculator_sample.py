532ded9ede18da043fbec8cb3e8f9cb9b6c19ea1
Shihas Backer <shihasbacker07@gmail.com>
2020-07-27 21:04:35 +0530

    screen

--- calculator_sample.py
+++ calculator_sample.py
@@ -3,12 +3,15 @@ window=Tk()
 window.geometry("500x500")
 window.title("calculator")
 window.configure(bg="red")
-button=Button(text="ok",width=30,height=30)
-label=Label(window,text="welcome")
+
+screen = Entry(window, font=('arial', 17, 'bold'), width=28, justify=RIGHT)
+screen.grid(row=0, column=0, columnspan=5, pady=1)
+screen.bind("<Key>", lambda e: "break")     # Disable characters from keyboard
+screen.insert(0, "0")
+
 
 def hello():
     print("button clicked")
 
-button.pack()
-label.pack()
+
 window.mainloop()
